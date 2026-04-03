543e697b9c8abcb54ba97cae9999dc040a85783d
Hanna Vretby <hr222sy@student.lnu.se>
2026-04-03 17:38:34 +0200

    feat: add AuthenticationRouter with GitHub OAuth routes

--- backend/src/routes/AuthenticationRouter.js
+++ backend/src/routes/AuthenticationRouter.js
@@ -6,20 +6,28 @@
  */
 
 import { Router } from 'express'
+import { AuthenticationController } from '../controllers/AuthenticationController.js'
 
 /**
- * Handles authentication routes.
+ * Handles authentication routes for OAuth flow.
  */
 export class AuthenticationRouter {
   #router
+  #controller
 
-  constructor () {
+  /**
+   * @param {AuthenticationController} [controller] - Injected for testing.
+   */
+  constructor (controller = new AuthenticationController()) {
     this.#router = Router()
+    this.#controller = controller
     this.#initializeRoutes()
   }
 
   #initializeRoutes () {
-    // Routes will be added here
+    this.#router.get('/github/login', (req, res) => this.#controller.login(req, res))
+    this.#router.get('/github/callback', (req, res, next) => this.#controller.callback(req, res, next))
+    this.#router.get('/logout', (req, res, next) => this.#controller.logout(req, res, next))
   }
 
   /**
